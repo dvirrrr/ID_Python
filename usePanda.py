0f3879f24331640f8921d6de09e65590ec5fd619
dvirrrr <127685226+dvirrrr@users.noreply.github.com>
2024-03-19 21:42:29 +0200

    loop

--- usePanda.py
+++ usePanda.py
@@ -1,1 +1,16 @@
+# Itay Dvir
 import pandas as pd
+
+
+def main():
+
+    # https://www.kaggle.com/datasets/justinas/nba-players-data?resource=download
+    file_path = 'all_seasons.csv'
+
+    df = pd.read_excel(file_path)
+    desc = df.describe()
+    print(desc)
+
+
+if __name__ == '__main__':
+    main()
